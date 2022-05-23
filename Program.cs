2139d77fe937182dc12ec96f117f9058e10f67b2
esteban <esteban030991@gmail.com>
2022-05-23 01:29:12 -0300

    abstraiendo en C#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using abstraindo_um_jogo_de_rpg.src.Entities;
+
+namespace abstraindo_um_jogo_de_rpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arus hero = new Arus("Arus", 23, "knight");
+        
+
+            Console.Write(hero.ToString());
+        }
+    }
+}
---
+++ src/Entities/Arus.cs
@@ -0,0 +1,17 @@
+namespace abstraindo_um_jogo_de_rpg.src.Entities
+{
+    public class Arus
+    {
+            public Arus(string Name,int level, string HeroType )
+            {
+                this.Name = Name;
+                this.level = level;
+                this.HeroType = HeroType;
+                
+            }
+        public string Name;
+        public int level;
+        public string HeroType;
+        
+    }
+}
